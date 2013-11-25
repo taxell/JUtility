2bf0cfa755b397d2ff50439f710d3ef85a961837
Tobias Axell <tobias.axell@gmail.com>
2013-11-25 18:50:58 +0100

    Added javadoc to PolynomialFunction.java

--- src/nu/xell/util/math/PolynomialFunction.java
+++ src/nu/xell/util/math/PolynomialFunction.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import nu.xell.util.functional.Function1;
 
 /**
+ * A polynomial function.
  * 
  * @author Tobias Axell
  */
@@ -13,8 +14,15 @@ public class PolynomialFunction implements Function1<Double, Double> {
 	private double[] coefficients;
 	
 	/**
+	 * Constructor for PolynomialFunction.
 	 * 
-	 * @param coefficients
+	 * @param coefficients The coefficients for the polynomial function.
+	 *   				   <code>coefficients[i]</code> will be the coefficient 
+	 *   				   for the <i>i</i>-th degree variable. So the size of the
+	 *   				   coefficients array decides the degree of the polynomial
+	 *   				   function. Example:
+	 *   				   If <code>coefficients = {1, 2, 3}</code> then the function
+	 *   				   will be <i>f(x) = 3x<sup>2</sup> + 2x + 1</i>
 	 */
 	public PolynomialFunction(double[] coefficients) {
 		if(coefficients.length < 1){
@@ -25,17 +33,19 @@ public class PolynomialFunction implements Function1<Double, Double> {
 	}
 	
 	/**
+	 * Gives the degree of the polynomial function.
 	 * 
-	 * @return
+	 * @return The degree of the polynomial.
 	 */
 	public int degree(){
 		return coefficients.length - 1;
 	}
 	
 	/**
+	 * Gives the coefficient of the term of a specified degree.
 	 * 
-	 * @param degree
-	 * @return
+	 * @param degree The degree of the term to give the coefficient for.
+	 * @return The coefficient of the specified term.
 	 */
 	public double getCoefficient(int degree){
 		return coefficients[degree];
@@ -49,5 +59,17 @@ public class PolynomialFunction implements Function1<Double, Double> {
 			sum += coefficients[i] * Math.pow(x, i);
 		}
 		return sum;
+	}
+	
+	@Override
+	public String toString(){
+		String s = "f(x) = ";
+		
+		for(int i = coefficients.length - 1; i > 0; i--){
+			s += coefficients[i] + "x^" + i + " + ";
+		}
+		s += coefficients[0];
+		
+		return s;
 	}
 }
